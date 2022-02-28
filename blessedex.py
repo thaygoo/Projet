55fc490aeacbd21864dca0bc3453291a1ea1018d
Thaygoo <hugo.burton@student.unamur.be>
2022-02-28 11:13:02 +0100

    V0

--- blessedex.py
+++ blessedex.py
@@ -1,38 +1,30 @@
 import blessed, random, time
 term = blessed.Terminal()
 
+#https://www.fileformat.info/info/unicode/block/box_drawing/images.htm
+
+color = term.red
+lenght = 20
 # clear screen and hide cursor
 print(term.home + term.clear + term.hide_cursor)
 
-# define position of the symbol
-row = int(term.height/2)
-col = int(term.width/2)
+#header
+print(color + '\u2554'+3*'\u2550'+(int(lenght)-1)*('\u2566'+3*'\u2550')+'\u2557')
+print(color + '\u2551'+lenght*(3*'\u0020'+'\u2551'))
+#body
+for i in range(lenght-1):
+    print(color + '\u2560'+(int(lenght)-1)*(3*'\u2550'+'\u256C')+3*'\u2550'+'\u2563')
+    print(color + '\u2551'+lenght*(3*'\u0020'+'\u2551'))
+#foot
+print(color + '\u255A'+3*'\u2550'+(int(lenght)-1)*('\u2569'+3*'\u2550')+'\u255D')
+
+
 
 # move cursor + change color of symbol + change color of background + symbol + back to normal
-print(term.move_yx(row, col) + term.green + term.on_rosybrown2 + '●' + term.normal, end='', flush=True)
+#print(term.move_yx(row, col) + term.green + term.on_rosybrown2 + '●' + term.normal, end='', flush=True)
 
-# move symbol forever
-while True:
-    # wait a fraction of second
-    time.sleep(0.05)
+# move cursor + change color of symbol + change color of background + single space + back to normal
+#print(term.move_yx(row, col) + term.on_skyblue + ' ' + term.normal, end='', flush=True)
 
-    # move cursor + change color of symbol + change color of background + single space + back to normal
-    print(term.move_yx(row, col) + term.on_skyblue + ' ' + term.normal, end='', flush=True)
-
-    # update position symbol
-    move = random.choice(((-1, 0), (1, 0), (0, -1), (0, 1)))
-
-    row += move[0]
-    if row < 0:
-        row = 0
-    if row >= term.height:
-        row = term.height-1
-
-    col += move[1]
-    if col < 0:
-        col = 0
-    if col >= term.width:
-        col = term.width-1
-    
-    # move cursor + change color of symbol + change color of background + symbol + back to normal
-    print(term.move_yx(row, col) + term.red + term.on_rosybrown2 + '●' + term.normal, end='', flush=True)
+# move cursor + change color of symbol + change color of background + symbol + back to normal
+#print(term.move_yx(row, col) + term.red + term.on_rosybrown2 + '●' + term.normal, end='', flush=True)
