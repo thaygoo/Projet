be1dd97722bca077812c8ac0ccf13e2b2a0cd1f9
Thaygoo <hugo.burton@student.unamur.be>
2022-03-01 16:40:14 +0100

    Add config def, missing foods

--- test.py
+++ test.py
@@ -4,31 +4,45 @@ term = blessed.Terminal()
 
 def board(width, height, color):
     # background + cursor + clear + hide cursor
-    print(term.on_darkslategray4 + term.home + term.clear + term.hide_cursor)
+    print(term.on_darkslategray4 + term.home + term.clear)
 
     #header
-    print(term.center(color + '\u2554' + 3 * '\u2550' + (int(width) - 1) * ('\u2566' + 3 * '\u2550') + '\u2557'))
-    print(term.center(color + '\u2551' + width * (3 * '\u0020' + '\u2551')))
+    print(term.center(color + '╔' + 3 * '═' + (int(width) - 1) * ('╦' + 3 * '═') + '╗'))
+    print(term.center(color + '║' + width * (3 * ' ' + '║')))
 
     #body
     for i in range(height - 1):
-        print(term.center(color + '\u2560' + (int(width) - 1) * (3 * '\u2550' + '\u256C') + 3 * '\u2550' + '\u2563'))
-        print(term.center(color + '\u2551' + width * (3 * '\u0020' + '\u2551')))
+        print(term.center(color + '╠' + (int(width) - 1) * (3 * '═' + '╬') + 3 * '═' + '╣'))
+        print(term.center(color + '║' + width * (3 * ' ' + '║')))
         
     #foot
-    print(term.center(color + '\u255A' + 3 * '\u2550' + (int(width) - 1) * ('\u2569' + 3 * '\u2550') + '\u255D'))
+    print(term.center(color + '╚' + 3 * '═' + (int(width) - 1) * ('╩' + 3 * '═') + '╝'))
 
 def config(file):
-    config = {1:{}, 2:{}, "food":{}, 'map':()}
+    config = {1:{'alpha': '', 'omega': '', 'normal': []}, 2:{'alpha': '', 'omega': '', 'normal': []}, "food":{}, 'map':()}
+
     with open(file) as fp:
         line = fp.readline()
         while line:
-            if line.strip() == 'map:':
+            if 'map:' in line.strip():
                 line = fp.readline()
-                print(line)
-                config['map'] = line.rsplit(" ")
+                line = line.rsplit(" ")
+                line[1] = line[1].rstrip("\n")
+                config['map'] = line
+
+            elif 'werewolves:' in line.strip():
+                line = fp.readline()
+                while 'foods:' not in line.strip():
+                    if 'alpha' in line.strip():
+                        config[int(line.strip()[0])]['alpha'] = [int(line.strip().split(' ').pop(1)), int(line.strip().split(' ').pop(2)), int(100)]
+                    elif 'omega' in line.strip():
+                        config[int(line.strip()[0])]['omega'] = [int(line.strip().split(' ').pop(1)), int(line.strip().split(' ').pop(2)), int(100)]
+                    else:
+                        config[int(line.strip()[0])]['normal'].append([int(line.strip().split(' ').pop(1)), int(line.strip().split(' ').pop(2)), int(100)])
+                    line = fp.readline()
             line = fp.readline()
     return config
 
-#print(config('map.ano'))
-board(20,20,term.black)
+dic = config('map.ano')
+print('\n', dic[1], '\n\n', dic[2], '\n\n', dic['food'])
+#board(8,8,term.black)
