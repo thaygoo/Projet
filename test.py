cf5ec21404947f644e8846f1a219422a94d3f135
Thaygoo <hugo.burton@student.unamur.be>
2022-03-08 09:15:48 +0100

    #1

--- test.py
+++ test.py
@@ -33,18 +33,19 @@ def board(width, height, color): # REGLER LE SOUCIS DU Y
 
     #placing objects
     #Placing Alphas :
-
     for i in ['alpha', "α"], ['omega', "Ω"]:
         for j in [1, term.bold_red], [2, term.bold_green]:
             # display alpha and omega
             print(term.move_xy(*coordinate(*config('map.ano')[j[0]][i[0]][:2])) + j[1] + i[1])
-            #display energy
+            # display energy
             print(term.move_xy((coordinate(*config('map.ano')[j[0]][i[0]][:2])[0])-1, (coordinate(*config('map.ano')[j[0]][i[0]][:2])[1])+1) + j[1] + '%d' % (config('map.ano')[j[0]][i[0]][2]))
 
     # Placing normal wolves:
     for j in [1, "\u29BF", term.bold_red], [2, "\u29BF", term.bold_green]:
         for i in config('map.ano')[j[0]]['normal']:
+            # display normal wolves
             print(term.move_xy(*coordinate(*i[:2])) + j[2] + j[1])
+            # display energy of them
             print(term.move_xy((coordinate(*i[:2])[0])-1, (coordinate(*i[:2])[1])+1) + j[2] + '%d' % (i[2]))
 
     #place foods
@@ -55,12 +56,17 @@ def board(width, height, color): # REGLER LE SOUCIS DU Y
             # Display energy of foods
             print(term.move_xy((coordinate(*i[:2])[0])-1, (coordinate(*i[:2])[1])+1) + term.turquoise + '%d' % (i[2]))
     
+    display()
+    
 
 def coordinate(x, y):
     home = int(term.width/2) - int(((4 * 20)+1)/2)
     x = home+(2+(4*(x-1)))
     y = (y*2)-1
     return x, y
+
+def display():
+    print(term.normal + term.move_xy(int((4 * 20)+1), int((2 * 20)+1)) + ' ')
 
 def config(file):
     config = {
@@ -98,12 +104,10 @@ def config(file):
 
 #print(dic)
 
-#board(20,20,term.gold)
+board(20,20,term.gold)
 
 #x, y = coordinate(2,1)
 #print(term.move_xy(x, y) + term.on_normal + term.black + '\u29BB')
-
-#print(term.red + term.move_xy(100, 45) + '')
 
 """ for i in range(2000): 
     print(term.red + term.move_xy(10, 20) + "%d" % i)
